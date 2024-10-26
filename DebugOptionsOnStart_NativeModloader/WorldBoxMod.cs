8a0bcf4c0cf94c1e3462dc8a6f3f43245a0de4cd
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-10-26 13:33:28 +0300

    Добавьте файлы проекта.

---
+++ DebugOptionsOnStart_NativeModloader/WorldBoxMod.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace DebugOptionsOnStart_NativeModloader
+{
+    public class WorldBoxMod : MonoBehaviour
+    {
+        static bool initialized = false;
+
+        void Update()
+        {
+            if (!Config.gameLoaded) return;
+
+
+            if (!initialized)
+            {
+                init();
+            }
+
+            initialized = true;
+        }
+
+        void init()
+        {
+            DebugConfig.instance.debugButton.SetActive(true);
+            DebugConfig.setOption(DebugOption.PauseOnStart, true, true);
+            DebugConfig.setOption(DebugOption.ShowHiddenStats, true, true);
+            DebugConfig.setOption(DebugOption.SonicSpeed, true, true);
+            DebugConfig.setOption(DebugOption.Greg, true, true);
+            DebugConfig.setOption(DebugOption.RenderHoldingResources, true, true);
+        }
+    }
+}
